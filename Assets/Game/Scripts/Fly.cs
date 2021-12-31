8bcbab95e5afa76ef08bcfa9284495ce46806e9e
CihatCitak <cihatcitakc@gmail.com>
2022-01-01 01:37:05 +0300

    Fly mechanic added.

---
+++ Assets/Game/Scripts/Fly.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidbody2D;
+    [SerializeField] float jumpPower = 1f;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidbody2D.velocity = Vector2.up * jumpPower;
+    }
+}
